5eb2799e8d51dff5f4e7d6b5701b666e3a9dc7f2
DESKTOP-BLK8OS8\Doležalová <edolezalova@gmail.com>
2021-03-13 15:21:24 +0100

    no message

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
-
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
  *
  * @param {int} a
  * @param {int} b
  */
-function secti(a, b) {
-  let c = a + b;
-  return c;
+
+ function secti(a, b) {
+  let vysledek = a + b;
+  document.querySelector("#vysledek").innerHTML=vysledek;  
 }
 
 /**
  * Upozorní uživatele při spuštění.
  */
+
 function upozorni() {
   alert("Gratulace, právě jsi spustila tuto funkci!");
+  console.log("Gratulace, právě si spustila tuto funkci!");
+  document.querySelector(".ctverecek").innerHTML="Gratulace, právě si spustila tuto funkci!";
+  document.querySelector(".ctverecek").style.width="500px";
+}
+
+
+function zmenBarvu() {
+  document.querySelector(".ctverecek").style.backgroundColor = "green"; 
 }
